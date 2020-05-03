de28636f80101e16254effc413c6e379361f7824
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-03 12:57:05 +0600

    numbersRecyclerAdapter

--- app/src/main/java/com/example/android/miwok/NumbersRecyclerAdapter.java
+++ app/src/main/java/com/example/android/miwok/NumbersRecyclerAdapter.java
@@ -1,4 +1,25 @@
 package com.example.android.miwok;
 
-public class NumbersRecyclerAdapter {
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NumbersRecyclerAdapter extends RecyclerView.Adapter<NumbersRecyclerAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public NumbersRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NumbersRecyclerAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
