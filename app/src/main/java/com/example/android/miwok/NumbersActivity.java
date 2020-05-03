323602d9fee0122b4393ac3f098d07adcc73a7eb
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-03 10:17:57 +0600

    activity label change

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,11 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
+
+    ArrayList<String> numbers = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
