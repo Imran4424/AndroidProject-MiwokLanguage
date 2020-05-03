68c61060b8bf17d452f344b095dd6d680e703841
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-03 10:27:36 +0600

    numbers activity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,17 +2,21 @@ package com.example.android.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
+    LinearLayout rootView;
     ArrayList<String> numbers = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        rootView = (LinearLayout) findViewById(R.id.verticalLinearLayout);
 
         numbers.add("one");
         numbers.add("two");
